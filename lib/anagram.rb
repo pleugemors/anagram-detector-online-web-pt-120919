149981d36704f251f7e49d56d6a457eaa396f057
Paul Leugemors <pleugemors@yahoo.com>
2019-12-11 18:54:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram 
+  def initialize(string)
+    @anagram = string 
+  end
+  
+  attr_accessor :word, :name 
+  
+  def match(string)
+    side1 = @anagram.split("")
+    string.select{|word| word.split("").sort == side1.sort}
+    
+  end
+  
+end
